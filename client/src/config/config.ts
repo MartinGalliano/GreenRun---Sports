f049ec27b77a23cd5a11bfe7317173ab019fdc9b
Jorge Martin Galliano <martin.galliano@hotmail.com>
2021-12-30 18:37:49 -0300

    login complete

---
+++ client/src/config/config.ts
@@ -0,0 +1,13 @@
+const config = {
+    firebase: {
+        apiKey: "AIzaSyDBuZM_HBqKeVyml3DWMqKvKtLzaUmitmI",
+        authDomain: "green-run-31555.firebaseapp.com",
+        projectId: "green-run-31555",
+        storageBucket: "green-run-31555.appspot.com",
+        messagingSenderId: "724346025080",
+        appId: "1:724346025080:web:590121a22abff95e74298d",
+        measurementId: "G-M24NF84LDR"
+    }
+}
+
+export default config;
